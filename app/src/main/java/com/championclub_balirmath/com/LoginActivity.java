0514e96d9d3192df7391accca2a50c9f24814d55
Sand Man <rajeshbhadra62@gmail.com>
2023-02-04 20:41:13 +0530

    login screen is done

--- app/src/main/java/com/championclub_balirmath/com/LoginActivity.java
+++ app/src/main/java/com/championclub_balirmath/com/LoginActivity.java
@@ -3,20 +3,38 @@ package com.championclub_balirmath.com;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.championclub_balirmath.com.databinding.ActivityLoginBinding;
+
+import java.util.zip.Inflater;
 
 public class LoginActivity extends AppCompatActivity {
-    CardView cardView;
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         //Removing top bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Adding custom card background in card view
-        cardView = findViewById(R.id.cardBackgroundId);
-        cardView.setBackgroundResource(R.drawable.login_card_background_shape);
+        binding.cardBackgroundId.setBackgroundResource(R.drawable.login_card_background_shape);
+        // Redirect to home page
+        binding.joinRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
