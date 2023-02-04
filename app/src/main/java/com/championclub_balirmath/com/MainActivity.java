0f8662ea91a66785b1b773fdbaf72f2c64ff2cf4
Sand Man <rajeshbhadra62@gmail.com>
2023-02-04 14:12:16 +0530

    splash screen designed is on going

--- app/src/main/java/com/championclub_balirmath/com/MainActivity.java
+++ app/src/main/java/com/championclub_balirmath/com/MainActivity.java
@@ -3,6 +3,7 @@ package com.championclub_balirmath.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
