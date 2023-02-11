f1522596c5b694d9cba5416adc8c2c0a7a2db132
Sand Man <rajeshbhadra62@gmail.com>
2023-02-11 10:11:34 +0530

    all activities are connecting with home page

--- app/src/main/java/com/championclub_balirmath/com/Activity/HomeActivity.java
+++ app/src/main/java/com/championclub_balirmath/com/Activity/HomeActivity.java
@@ -5,7 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.championclub_balirmath.com.Adapter.EventCardAdapter;
 import com.championclub_balirmath.com.Model.EventCardModel;
@@ -29,13 +28,44 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void buttonCLicked() {
-        binding.messageBtnId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, ChattingActivity.class);
-                startActivity(intent);
-            }
+
+        binding.messageBtnId.setOnClickListener(v -> { // This button for redirecting , Home page to Message page
+            Intent intent = new Intent(HomeActivity.this, ChattingActivity.class);
+            startActivity(intent);
         });
+
+        binding.eventBtnId.setOnClickListener(v -> { // This on click listener is to redirect to event activity
+            Intent intent = new Intent(HomeActivity.this, EventsActivity.class);
+            startActivity(intent);
+        });
+
+        binding.proflleBtn.setOnClickListener(v -> { // This button for redirecting home page to Member profile page
+            Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        binding.ClubUsers.setOnClickListener(v -> { // This button for redirecting homepage to ClubUsers page
+            Intent intent = new Intent(HomeActivity.this, MembersActivity.class);
+            startActivity(intent);
+        });
+
+
+        binding.NoteTakingBtn.setOnClickListener(v -> { // This button for redirecting homepage to Note page
+            Intent intent = new Intent(HomeActivity.this, NotesActivity.class);
+            startActivity(intent);
+        });
+
+        binding.SettingBtnId.setOnClickListener(v -> { // This button for redirecting homepage to Settings page
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        binding.DonateBtnId.setOnClickListener(v -> { // This button for redirecting homepage to Settings page
+            Intent intent = new Intent(HomeActivity.this, DonateActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 
     public void cardRecView() {
--- app/src/main/java/com/championclub_balirmath/com/Adapter/EventCardAdapter.java
+++ app/src/main/java/com/championclub_balirmath/com/Adapter/EventCardAdapter.java
@@ -1,14 +1,18 @@
 package com.championclub_balirmath.com.Adapter;
 
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.championclub_balirmath.com.Activity.KnowMoreActivity;
 import com.championclub_balirmath.com.Model.EventCardModel;
 import com.championclub_balirmath.com.R;
 
@@ -34,6 +38,9 @@ public class EventCardAdapter extends RecyclerView.Adapter<EventCardAdapter.MyVi
         holder.EventName.setText(eventCardModels.get(position).getEventName());
         holder.EventOrganiserName.setText(eventCardModels.get(position).getEventOrganiserName());
         holder.EventDate.setText(eventCardModels.get(position).getEventDate());
+        holder.knowMore.setOnClickListener(v -> {
+            Toast.makeText(holder.EventDate.getContext(), "Yes clicked...", Toast.LENGTH_SHORT).show();
+        });
     }
 
     @Override
@@ -43,12 +50,14 @@ public class EventCardAdapter extends RecyclerView.Adapter<EventCardAdapter.MyVi
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView EventName, EventOrganiserName, EventDate;
+        CardView knowMore;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             EventName = itemView.findViewById(R.id.eventTitleId);
             EventDate = itemView.findViewById(R.id.eventDateId);
             EventOrganiserName = itemView.findViewById(R.id.eventOrganiserNameId);
+            knowMore = itemView.findViewById(R.id.knowMoreBtnId);
         }
     }
 }
