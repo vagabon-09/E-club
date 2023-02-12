bf648fc45b9ef4e6ed4429eff8b039be4d9bd5c6
Sand Man <rajeshbhadra62@gmail.com>
2023-02-12 20:20:28 +0530

    profile page designing...

--- app/src/main/java/com/championclub_balirmath/com/Activity/LoginActivity.java
+++ app/src/main/java/com/championclub_balirmath/com/Activity/LoginActivity.java
@@ -1,18 +1,29 @@
 package com.championclub_balirmath.com.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.championclub_balirmath.com.R;
 import com.championclub_balirmath.com.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +32,55 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//Removing top bar
         binding.cardBackgroundId.setBackgroundResource(R.drawable.login_card_background_shape);// Adding custom card background in card view
-        // Redirect to home page
+        mAuth = FirebaseAuth.getInstance();
         binding.joinRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+                Toast.makeText(LoginActivity.this, "Clicked on joining request", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.loginBtnId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseLogin();
             }
         });
     }
+
+    private void FirebaseLogin() {
+        String email = binding.LoginEmailId.getText().toString();
+        String password = binding.loginPasswordId.getText().toString();
+        if (!email.isEmpty()) {
+            if (!password.isEmpty()) {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please enter your password..", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Please enter your email id...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
 }
